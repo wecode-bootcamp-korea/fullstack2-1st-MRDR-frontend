6656d3e9d972ac2ead38acebfa502e2acf949af4
DeVeom <deveomm@gmail.com>
2021-10-03 20:59:00 +0900

    Add: - 상품 상세 페이지 Products 컴포넌트를 ImagesContainer, InfoContainer, AdditionalInfoContainer 하위 컴포넌트로 분리 및 생성 - Products와 하위 컴포넌트 전체 레이아웃 구현(컴포넌트 별 scss 파일 생성) - common.scss, mixins.scss 파일은 구현 X -> 팀적으로 의논 필요

---
+++ src/pages/Products/ImagesContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ImagesContainer.scss';
+
+class ImagesContainer extends React.Component {
+  render() {
+    return <div className="ImagesContainer">ImagesContainer</div>;
+  }
+}
+
+export default ImagesContainer;
--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import ImagesContainer from './ImagesContainer';
+import InfoContainer from './InfoContainer';
+import AdditionalInfoContainer from './AdditionalInfoContainer';
 import './Products.scss';
 
 class Products extends React.Component {
   render() {
-    return <div className="Products">Products</div>;
+    return (
+      <div className="Products">
+        <div className="mainInfoWrapper">
+          <ImagesContainer />
+          <InfoContainer />
+        </div>
+        <AdditionalInfoContainer />
+      </div>
+    );
   }
 }
 
