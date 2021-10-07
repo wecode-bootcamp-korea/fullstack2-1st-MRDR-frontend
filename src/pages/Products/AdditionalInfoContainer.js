6a38e8a06ea62c786a51ac9af73fc52295acde98
DeVeom <deveomm@gmail.com>
2021-10-07 15:04:56 +0900

    Add: 추가 정보 컨테이너 구현 initial commit

--- src/pages/Products/AdditionalInfoContainer.js
+++ src/pages/Products/AdditionalInfoContainer.js
@@ -4,7 +4,9 @@ import './AdditionalInfoContainer.scss';
 class AdditionalInfoContainer extends React.Component {
   render() {
     return (
-      <div className="AdditionalInfoContainer">AdditionalInfoContainer</div>
+      <div className="AdditionalInfoContainer">
+        상세 페이지 추가 정보 구현 시작!
+      </div>
     );
   }
 }
