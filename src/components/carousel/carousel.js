55cc52cf8983336bc880d9614c95651bbca9e8e2
WY K <wyk@WYui-MacBookPro.local>
2021-10-09 21:07:54 +0900

    Add : Carousel

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -10,30 +10,73 @@ class Carousel extends React.Component {
   constructor() {
     super();
     this.state = {
-      currentSlide : 0
-    }
+      currentIndex: 0,
+      totalSlides: 4,
+    };
   }
+
+  componentDidMount() {
+    this.timer = setInterval(this.next, 4000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  updateSlider = ({ target: { currentIndex, value } }) => {
+    this.setState({ [currentIndex]: value });
+  };
+
+  next = () => {
+    const { currentIndex, totalSlides } = this.state;
+    if (currentIndex < totalSlides - 1) {
+      this.setState({
+        currentIndex: this.state.currentIndex + 1,
+      });
+    } else if (currentIndex === totalSlides - 1) {
+      this.setState({
+        currentIndex: 0,
+      });
+    }
+  };
+
+  prev = () => {
+    const { currentIndex, totalSlides } = this.state;
+    if (currentIndex > 0) {
+      this.setState({
+        currentIndex: currentIndex - 1,
+      });
+    } else if (currentIndex === 0) {
+      this.setState({
+        currentIndex: totalSlides - 1,
+      });
+    }
+  };
+
   render() {
     return (
-      <div className="carousel">
-        <div className="slideWrap">
-          <ul 
-            style{{
-              transform : `translateX(-${1300 * this.state.currentSlide}px)`}}>
-            <li className="cell">0</li>
-            <li className="cell">1</li>
-            <li className="cell">2</li>
-            <li className="cell">3</li>
-            <li className="cell">4</li>
-          </ul>
-        </div>
-        <div className="slideBtn">
-          <button className="prevBtn">
-            <FontAwesomeIcon icon={faChevronLeft} className="previousIcon" />
+      <div className="carouselContainer">
+        <div className="carouselWrapper">
+          <button className="leftArrow" onClick={this.prev}>
+            <FontAwesomeIcon icon={faChevronLeft} />
           </button>
-          <button className="nextBtn">
-            <FontAwesomeIcon icon={faChevronRight} className="nextIcon" />
+          <button className="rightArrow" onClick={this.next}>
+            <FontAwesomeIcon icon={faChevronRight} />
           </button>
+
+          <div className="carouselcontainerWrapper">
+            <div
+              className="carouselContent"
+              style={{
+                transform: `translateX(-${this.state.currentIndex * 100}%)`,
+              }}
+            >
+              <img src="/image/1.png" alt="img1" />
+              <img src="/image/2.png" alt="img2" />
+              <img src="/image/3.png" alt="img3" />
+              <img src="/image/4.png" alt="img4" />
+            </div>
+          </div>
         </div>
       </div>
     );
